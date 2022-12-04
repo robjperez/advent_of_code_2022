4fb0ae66f7cc074d5bcabfabecc35012189fd898
Roberto <robj.perez@gmail.com>
2022-12-04 18:37:12 +0100

    part 2 of day 1

--- racket/1/1.rkt
+++ racket/1/1.rkt
@@ -4,15 +4,25 @@
 (define input (map string->number (file->lines "input1.txt")))
 ; (define input '(1 2 #f 6 #f 1 1))
 
-(apply max   
-  (foldl 
+(define (sum-calories data)
+  (foldl
     (lambda (element acc)
       (if (number? element)
         (cons (+ (first acc) element) (cdr acc))
         (cons 0 acc)
-      )        
+      )
     )
     '(0)
-    input
-  )  
+    data
+  )
 )
+
+(define added-list (sum-calories input))
+
+(println "Part 1 - Max sum of calories")
+(apply max added-list)
+
+(println "Part 2 - Sum of the top 3")
+(apply + (take (sort added-list >) 3))
+
+
